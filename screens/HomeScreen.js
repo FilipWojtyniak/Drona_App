3bd2941f04475ddf8a86533d18ed4dba34524139
Filip <filipwojtyniak@gmail.com>
2022-04-19 20:18:07 +0200

    First build

---
+++ screens/HomeScreen.js
@@ -0,0 +1,144 @@
+import { StyleSheet, Text, View, SafeAreaView,ImageBackground, Image, TouchableOpacity } from 'react-native'
+import React from 'react'
+
+const HomeScreen = () => {
+  return (
+    <ImageBackground source={require('../assets/Background.jpg')} style={{width: '100%', height: '100%'}}>
+      <SafeAreaView>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <View style={styles.logo}>
+            <Image source={require('../assets/LOGO.png')}/>
+            <View style={styles.buttonsContainer}>
+            <TouchableOpacity>           
+                <Image  source={require('../assets/HomeButton.png')}/>
+           </TouchableOpacity>
+           <TouchableOpacity>
+                <Image source={require('../assets/SearchButton.png')}/>
+           </TouchableOpacity>
+           <TouchableOpacity>           
+                <Image source={require('../assets/ProfileButton.png')}/>
+           </TouchableOpacity> 
+           <TouchableOpacity>           
+                <Image  source={require('../assets/MenuButton.png')}/>
+           </TouchableOpacity>          
+          </View>
+          </View>
+          </View>
+          </View>
+          <View style={styles.textContainer}>
+            <Text style={styles.text}> The drones of </Text>
+            <Text style={styles.text}> our future. </Text>
+          <View style={styles.textContainer_2}>
+            <Text style={styles.text_2}> Amet minim mollit non deserunt </Text>
+            <Text style={styles.text_2}> ullamco est sit aliqua dolor do  </Text>
+            <Text style={styles.text_2}> amet sint. Velit officia consequat </Text>
+            <Text style={styles.text_2}> duis enim velit mollit.  </Text>
+            <Text style={styles.text_2}> Exercitation veniam consequat </Text>
+            <Text style={styles.text_2}> sunt nostrud amet.  </Text>
+          </View>
+          </View>
+          <View style={styles.buyButtonContainer}>
+           <TouchableOpacity style={styles.buyButton}>
+             <Text style={styles.buyText}>Buy Now</Text>           
+           </TouchableOpacity>
+          </View>
+          <View style={styles.learnButtonContainer}>
+           <TouchableOpacity style={styles.learnButton}>
+             <Text style={styles.learnText}>Learn more</Text>           
+           </TouchableOpacity>
+          </View>
+      </SafeAreaView>
+    </ImageBackground>
+  )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+  },
+  header:{
+  width:'100%',
+  height: 65,
+  backgroundColor: 'rgba(255, 255, 255, 0.5)',
+  borderBottomLeftRadius: 10,
+  borderBottomRightRadius: 10,
+  },
+  logo:{
+    flexDirection: 'row',
+    alignItems:'flex-start',
+    top:4,
+    left:11
+  },
+  buttonsContainer:{
+    flexDirection: 'row',
+    justifyContent:'space-between',
+    flex: 0.6,
+    left: 80,
+    top:14,
+  },
+  textContainer:{
+    alignItems:'center',
+    height:'100%',
+    top: 231
+
+  },
+  text:{
+    color:'#AD4473',
+    fontSize: 36,
+    fontStyle: 'italic',
+    fontFamily: Platform.OS === 'ios' ? 'Verdana' : 'serif',
+    textShadowColor: '#696969',
+    textShadowOffset: { width:2 ,height:2 },
+    textShadowRadius: 6,
+  },
+  textContainer_2:{
+    height: '100%',
+    alignItems:'center',
+    top: 40
+  },
+  text_2:{
+    fontSize: 20,
+    height: 33
+  },
+  buyButtonContainer:{
+    position:'absolute',
+    top: Platform.OS === 'ios' ? 700 : 600,
+    left: 30,
+    
+  },
+  buyButton:{
+    backgroundColor: 'rgba(41, 130, 146, 1)',
+    alignItems: 'center',
+    justifyContent:'center',
+    height: 60,
+    width:140,
+    borderRadius: 10
+  },
+  learnButtonContainer:{
+    position:'absolute',
+    top: Platform.OS === 'ios' ? 700 : 600,
+    right: 30,
+  },
+  learnButton:{
+    alignItems: 'center',
+    justifyContent:'center',
+    height:60,
+    width:140,
+    borderRadius: 10,
+    borderColor: 'rgba(41, 130, 146, 1)',
+    borderWidth: 3,
+  },
+  learnText:{
+    color: 'rgba(41, 130, 146, 1)',
+    fontSize: 20,
+    fontFamily: Platform.OS === 'ios' ? 'Verdana' : 'serif',
+  },
+  buyText:{
+    color: '#FFF',
+    fontSize: 20,
+    fontFamily: Platform.OS === 'ios' ? 'Verdana' : 'serif',
+  }
+})
---
+++ screens/LoginScreen.js
@@ -0,0 +1,126 @@
+import { StyleSheet, Text, View, KeyboardAvoidingView, TextInput, TouchableOpacity, ImageBackground  } from 'react-native'
+import React from 'react'
+import { useState, useEffect } from 'react'
+import { auth } from '../firebase'
+import { useNavigation } from '@react-navigation/core';
+
+
+function LoginScreen() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  
+  const navigation = useNavigation();
+
+  useEffect(() => {
+   const unsubscribe =  auth.onAuthStateChanged(user =>{
+      if(user){
+        navigation.replace('Home')
+      }
+    })
+    return unsubscribe;
+  },[])
+
+  const handleSingUp = () => {
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then(userCredentials => {
+        const user = userCredentials.user;
+      })
+      .catch(error => alert(error.message))
+  }
+
+ const handleLogin = () => {
+   auth.signInWithEmailAndPassword(email, password)
+   .then(userCredentials =>{
+     const user = userCredentials.user;
+     console.log('Logged', user.email);
+   })
+   .catch(error => alert(error.message))
+ }
+
+  return (
+    <ImageBackground source={require('../assets/Background.jpg')} style={{width: '100%', height: '100%'}}>
+       <KeyboardAvoidingView style={styles.container}>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            value= {email}
+            onChangeText= {text => setEmail(text)}
+            placeholder="Email" />
+          <TextInput
+            style={styles.input}
+            value= {password}
+            onChangeText= {text => setPassword(text)}
+            secureTextEntry
+            placeholder="Password" />
+        </View>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={[styles.buttonLogin, styles.bothButtons]} onPress ={handleLogin}>
+            <Text style={styles.login}>
+              Login
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.buttonRegister, styles.bothButtons]} onPress={handleSingUp}>
+            <Text style={styles.register}>
+              Register
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </KeyboardAvoidingView> 
+    </ImageBackground>
+  );
+}
+
+export default LoginScreen
+
+const styles = StyleSheet.create({
+  background:{
+    flex:1,
+  },
+  container:{
+    flex: 1,
+    alignItems: 'center',
+    justifyContent:'center'  
+  },
+  inputContainer:{
+    width: '80%',
+  
+  },
+  input:{
+    paddingHorizontal: 15,
+    backgroundColor: '#FFF',
+    paddingVertical: 10,
+    marginTop: 5,
+    borderRadius: 10
+  },
+  buttonContainer:{
+    width: '70%',
+    alignItems: 'center',
+    marginTop: 30,
+  },
+  buttonLogin:{
+    borderColor: '#298292',
+    borderWidth: 2,
+  },
+  buttonRegister:{
+    backgroundColor: '#298292',
+    marginTop: 5,
+  },
+  bothButtons:{
+    width: '70%',
+    alignItems:'center',
+    borderRadius: 10,
+    padding: 15,
+  },
+  login:{
+    fontWeight: '700',
+    fontSize: 16,
+    color: '#00bfff'
+  },
+  register:{
+    fontWeight: '700',
+    fontSize: 16,
+    color: '#FFF'
+  }
+
+})
